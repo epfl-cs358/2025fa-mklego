43f88d0d364711cfd2a21b8efb9e3c6e4e791646
Fedor Katanaev <fedor.katanaev@epfl.ch>
2025-10-30 14:18:03 +0100

    finished the initial rasterization code

--- frontend/projects/slicer-voxelizer/src/main/java/edu/epfl/mklego/slicer/voxelizer/Voxelizer.java
+++ frontend/projects/slicer-voxelizer/src/main/java/edu/epfl/mklego/slicer/voxelizer/Voxelizer.java
@@ -1,6 +1,7 @@
 package edu.epfl.mklego.slicer.voxelizer;
 
 import edu.epfl.mklego.objloader.Mesh;
+import edu.epfl.mklego.objloader.Mesh.Triangle;
 import javafx.geometry.Point3D;
 import java.util.Random;
 
@@ -13,6 +14,9 @@ public class Voxelizer {
     private static int verticalVoxelHeight = 100;
     private static int rayLength = horizontalVoxelSize * 22;      // max range you would need to go from one end to the other end of the field
     private static int numberOfPoints = 100;            // number of points used to approximate each voxel
+
+    static final double EPSILON = 1e-8;                 // can be changed. If too small, intersections may be ignored. If too large
+                                                        // false intersections can be recorded
 
     /**
      * Calculates the value for each voxel for whether we want to have a LEGO brick there
@@ -63,6 +67,64 @@ public class Voxelizer {
      * @param vector the vector that we want to get the intersections of
      */
     private static int getIntersections(Mesh mesh, Point3D origin, Point3D vector){
-        return 0;
+        int numberOfIntersections = 0;
+        
+        for (Triangle triangle : mesh.triangles()){
+            if (MollerTrumbore(origin, vector, triangle)){
+                numberOfIntersections++;
+            }
+        }
+
+        return numberOfIntersections;
+    }
+
+    /*
+     * use the MollerTrumbore algorithm to calculate whether ray intersects with a triangle
+     * code plan taken from wikipedia and then adapted
+     */
+    private static boolean MollerTrumbore(Point3D rayOrigin, Point3D rayVector, Triangle triangle){
+
+        Point3D vertex0 = triangle.p1();
+        Point3D vertex1 = triangle.p2();
+        Point3D vertex2 = triangle.p3();
+        Point3D edge1 = Point3D.ZERO;
+        Point3D edge2 = Point3D.ZERO;
+        Point3D h = Point3D.ZERO;
+        Point3D s = Point3D.ZERO;
+        Point3D q = Point3D.ZERO;
+        double a, f, u, v;
+        edge1 = vertex1.subtract(vertex0);
+        edge2 = vertex2.subtract(vertex0);
+        h = rayVector.crossProduct(edge2);
+        a = edge1.dotProduct(h);
+
+        if (a > -EPSILON && a < EPSILON) {
+            return false;    // This ray is parallel to this triangle.
+        }
+
+        f = 1.0 / a;
+        s = rayOrigin.subtract(vertex0);
+        u = f * (s.dotProduct(h));
+
+        if (u < 0.0 || u > 1.0) {
+            return false;
+        }
+
+        q = s.crossProduct(edge1);
+        v = f * rayVector.dotProduct(q);
+
+        if (v < 0.0 || u + v > 1.0) {
+            return false;
+        }
+
+        // At this stage we can compute t to find out where the intersection point is on the line.
+        double t = f * edge2.dotProduct(q);
+        if (t > EPSILON) // ray intersection
+        {
+            return true;
+        }
+
+        // This means that there is a line intersection but not a ray intersection.
+        return false;
     }
 }
